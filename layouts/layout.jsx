a58c31ab47b3828b848f98f65be42856e5f2dae9
Martin Maiaru <tincho_91_15@hotmail.com>
2024-06-10 00:49:22 +0200

    20

--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -3,14 +3,15 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import ContactForm from "../components/contactForm";
+import { Box } from "@chakra-ui/react";
 
 export default function RootLayout({ children }) {
   return (
-    <div>
+    <Box overflowX="hidden">
       <Navbar />
       {children}
       <ContactForm />
       <Footer />
-    </div>
+    </Box>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n:{
+    locales: ['it', 'es', 'en', 'pt'],
+    defaultLocale: 'it',
+  }
+}
 
 module.exports = nextConfig
